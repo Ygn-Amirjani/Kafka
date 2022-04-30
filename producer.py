87ebd6949b465d455742028f4a484b133d4877f9
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2022-04-30 18:39:16 +0430

    change for better

--- producer.py
+++ producer.py
@@ -1,20 +1,26 @@
 from kafka import KafkaProducer
 import time, json
 
-# It just needs to have at least one broker that will respond to a Metadata API Request. 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+# Load config file
+with open('config.json', mode='r') as config_file:
+    CONFIG = json.load(config_file)
 
-while True: 
-    """ continuously write messages to 'input' topic with epoch timestamp in ms """
+def send_message_to_input_topic():
+    # It just needs to have at least one broker that will respond to a Metadata API Request. 
+    Apache_kafka = f"{CONFIG.get('host')}:{CONFIG.get('port')}"
+    producer = KafkaProducer(bootstrap_servers=Apache_kafka)
 
-    # epoch time
-    current_time = int(time.time() * 1000)
+    while True: 
+        """ continuously write messages to 'input' topic with epoch timestamp in ms """
+        
+        # epoch time
+        current_time = int(time.time() * 1000)
+        # Publish a message to a topic.
+        msg = producer.send('input', json.dumps(current_time).encode('utf-8'))
+        # block until all asynchronous messages are sent
+        producer.flush()
+        # We send a message every 5 seconds .
+        time.sleep(5)
 
-    # Publish a message to a topic.
-    msg = producer.send('input', json.dumps(current_time).encode('utf-8'))
-
-    # block until all asynchronous messages are sent
-    producer.flush()
-
-    # We send a message every 5 seconds .
-    time.sleep(5)
+if __name__ == "__main__":
+    send_message_to_input_topic()
