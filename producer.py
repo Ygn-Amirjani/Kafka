e048a8a0f6d664b2293944a2e01f609b89c1fd35
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2022-04-30 19:37:01 +0430

    change for better

--- producer.py
+++ producer.py
@@ -6,14 +6,13 @@ with open('config.json', mode='r') as config_file:
     CONFIG = json.load(config_file)
 
 def send_message_to_input_topic():
+    """ continuously write messages to 'input' topic with epoch timestamp in ms """
+
     # It just needs to have at least one broker that will respond to a Metadata API Request. 
     Apache_kafka = f"{CONFIG.get('host')}:{CONFIG.get('port')}"
     producer = KafkaProducer(bootstrap_servers=Apache_kafka)
 
     while True: 
-        """ continuously write messages to 'input' topic with epoch timestamp in ms """
-        
-        # epoch time
         current_time = int(time.time() * 1000)
         # Publish a message to a topic.
         msg = producer.send('input', json.dumps(current_time).encode('utf-8'))
