27538944989103783484b6e8a20e4ddb56355365
Yegane Amirjani <yeganeh.a.m7979@gmail.com>
2022-05-12 22:42:27 +0430

    write producer code

---
+++ publish/producer.py
@@ -0,0 +1,20 @@
+from kafka import KafkaProducer
+import time, json
+
+def send_message_to_input_topic():
+    """ continuously write messages to 'input' topic with epoch timestamp in ms. """
+
+    # It just needs to have at least one broker that will respond to a Metadata API Request. 
+    producer = KafkaProducer(bootstrap_servers='kafka-service.default.svc.cluster.local:9092')
+
+    while True: 
+        current_time = int(time.time() * 1000)
+        # Publish a message to a topic.
+        producer.send('input', json.dumps(current_time).encode('utf-8'))
+        # block until all asynchronous messages are sent
+        producer.flush()
+        # We send a message every 5 seconds .
+        time.sleep(1)
+
+if __name__ == "__main__":
+    send_message_to_input_topic()
